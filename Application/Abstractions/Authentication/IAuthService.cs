e882cff9cb64aac1b0746b3a0dfbb29b9aaa9ed4
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-03-25 12:18:42 +0100

    fix

--- Application/Abstractions/Authentication/IAuthService.cs
+++ Application/Abstractions/Authentication/IAuthService.cs
@@ -7,5 +7,5 @@ public interface IAuthService
     Task<AuthResult> SignUpUserAsync(string email, string password, string roleName = "Member");
     Task<AuthResult> SignInUserAsync(string email, string password, bool rememberMe);
 
-    Task SignOutUserAsync();
+    Task SignOutUserAync();
 }
